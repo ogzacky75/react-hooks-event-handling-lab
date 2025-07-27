7a5be01dc83e0d82938c30f52ac8f868285643cb
ogzacky75 <ogzacky75@gmail.com>
2025-07-27 16:08:08 +0300

    finishing codegrade

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,13 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+            type="password"
+            onChange={()=> console.log('Entering password...')}
+            
+            />
+        </div>
     )
 }
 
